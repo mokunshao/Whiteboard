2bfaf482f9b13b467f2476e7430f6b0c18749cb0
mokunshao <1627510957@qq.com>
2018-08-17 08:40:09 +0800

    添加移动端支持

--- main.js
+++ main.js
@@ -18,13 +18,14 @@ window.onresize = function(){
 }
 
 
-// 画圆画线的函数
+// 画圆的函数
 function drawCircle(x,y){
 	context.beginPath();
 	context.arc(x,y,2,0,Math.PI*2);
 	context.fill();
 }
 
+// 画线的函数
 function drawLine(x1,y1,x2,y2){
 	context.beginPath();
 	context.moveTo(x1,y1);
@@ -34,10 +35,6 @@ function drawLine(x1,y1,x2,y2){
 	context.closePath;
 }
 
-
-
-// 起始点
-var lastPoint={x:undefined, x:undefined}
 
 
 // 允许创作
@@ -62,42 +59,97 @@ eraser.onclick = function(){
 }
 
 
-
-//鼠标按下后
-canvas.onmousedown = function(point){
-	var x = point.clientX;
-	var y = point.clientY;
-	creating = true;
-	if(usingPen===true){
-		drawCircle(x,y);
-		lastPoint = {x:x, y:y};
-	}
-	if(usingEraser === true){
-		context.clearRect(x,y,10,10)
-	}
-}
+// 执行监听用户行为
+userAction(canvas);	
 
 
-// 鼠标移动
-canvas.onmousemove = function(point){
-	var x = point.clientX;
-	var y = point.clientY;
-	if (creating === false){
-		return;
+//监听用户行为的函数
+function userAction(canvas){
+
+	if(document.body.ontouchstart===null){
+
+		// 按下鼠标
+		canvas.ontouchstart = function(point){
+			var x = point.touches[0].clientX;
+			var y = point.touches[0].clientY;
+			creating = true;
+			if(usingPen===true){
+				drawCircle(x,y);
+				lastPoint = {x:x, y:y};
+			}
+			if(usingEraser === true){
+				context.clearRect(x,y,10,10)
+			}
+		}
+
+		// 鼠标移动
+		canvas.ontouchmove = function(point){
+			var x = point.touches[0].clientX;
+			var y = point.touches[0].clientY;
+			if (creating === false){
+				return;
+			} 
+			if (usingEraser === true) {
+				context.clearRect(x,y,10,10)
+			} 
+			if(usingPen===true){
+				drawCircle(x,y);
+				var newPoint ={x:x, y:y};
+				drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
+				lastPoint = newPoint;
+			}
+		}
+
+		//松开鼠标
+		canvas.ontouchend = function(point){
+			creating = false;
+		}
 	} 
-	if (usingEraser === true) {
-		context.clearRect(x,y,10,10)
-	} 
-	if(usingPen===true){
-		drawCircle(x,y);
-		var newPoint ={x:x, y:y};
-		drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
-		lastPoint = newPoint;
+	
+	if(document.body.ontouchstart===undefined){
+
+		// 按下鼠标
+		canvas.onmousedown = function(point){
+			var x = point.clientX;
+			var y = point.clientY;
+			creating = true;
+			if(usingPen===true){
+				drawCircle(x,y);
+				lastPoint = {x:x, y:y};
+			}
+			if(usingEraser === true){
+				context.clearRect(x,y,10,10)
+			}
+		}
+
+		// 鼠标移动
+		canvas.onmousemove = function(point){
+			var x = point.clientX;
+			var y = point.clientY;
+			if (creating === false){
+				return;
+			} 
+			if (usingEraser === true) {
+				context.clearRect(x,y,10,10)
+			} 
+			if(usingPen===true){
+				drawCircle(x,y);
+				var newPoint ={x:x, y:y};
+				drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
+				lastPoint = newPoint;
+			}
+		}
+
+		//松开鼠标
+		canvas.onmouseup = function(point){
+			creating = false;
+		}
 	}
 }
 
 
-//松开鼠标
-canvas.onmouseup = function(point){
-	creating = false;
-}
+
+
+
+
+
